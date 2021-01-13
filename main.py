f457a871fe21407b570dd0edd5ba7172ec12dd24
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-01-13 15:25:51 +0100

    added forward pass to main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """
 - [x] load dataset (and/or create)
-- [ ] apply spacy pipeline
-- [ ] apply forward pass using DaBERT (save embedding + attention)
+- [x] apply spacy pipeline
+- [x] apply forward pass using DaBERT (save embedding + attention)
 - [ ] apply some kind of coref (not yet)
 - [ ] extract knowledge graph
+    - [x] create mapping betweeen bert and spacy
     - [ ] extract head tail pair
     - [ ] extract candidate facts
     - [ ] filter candidate facts
@@ -17,6 +18,9 @@ import transformers
 import datasets
 import spacy
 
+from spacy_parsing import spacy_preprocess
+from utils import forward_pass
+
 
 def input_to_dataset():
     def __gen_from_folder():
@@ -28,10 +32,65 @@ def input_to_dataset():
         d["text"].append(text)
         d["filename"].append(fn)
 
-    return datasets.Dataset.from_dict(ds)
+    return datasets.Dataset.from_dict(d)
+
+
+def __spacy_preprocess(batch):
+    preprocessed = spacy_preprocess(batch["text"], nlp=nlp)
+    d = {}
+    for doc in preprocessed:
+        for key in doc.keys():
+            skey = "spacy_" + key
+            if skey not in d:
+                d[skey] = []
+            d[skey].append(doc[key])
+    return d
+
+
+def doc_to_sent(batch):
+    """
+    Split a preprocessed document into sentences
+    """
+    d = {}
+    for key in batch.keys():
+        d[key] = []
+
+    for doc in range(len(batch["text"])):
+        max_sent = max(batch["spacy_sent_id"][doc])
+        i = 0
+        start_idx = 0
+        while i < max_sent:
+            i += 1
+            end_idx = batch["spacy_sent_id"][doc].index(i)
+
+            for k in d.keys():
+                # if in spacy add the sentence
+                if k.startswith("spacy"):
+                    d[k].append(batch[k][doc][start_idx:end_idx])
+                # if in text add the sentence using idx
+                elif k == "text":
+                    start = batch["spacy_token_character_span"][doc][start_idx][0]
+                    end = batch["spacy_token_character_span"][doc][end_idx - 1][1]
+                    sent = batch[k][doc][start:end]
+                    d[k].append(sent)
+                # add all metadata to all derived sentences
+                else:
+                    d[k].append(batch[k][doc])
+            start_idx = end_idx
+    return d
+
+
+def _forward_pass(batch):
+    return forward_pass(
+        batch["text"],
+        model=model,
+        tokenizer=tokenizer,
+        padding="max_length", max_length=128, truncation=True)
 
 
 if __name__ == '__main__':
+    batch_size = None
+    write_file = False
     # load tokenizers models and spacy pipe
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         "Maltehb/-l-ctra-danish-electra-small-cased")
@@ -41,5 +100,21 @@ if __name__ == '__main__':
 
     # load dataset using HF datasets
     ds = input_to_dataset()
+    if batch_size is None:
+        batch_size = len(ds)
+    ds = ds.map(__spacy_preprocess, batched=True, batch_size=batch_size)
 
-    # apply 
+    # write preprocessed for other tasks
+    if write_file:
+        pass
+        # write file append to ndjson
+
+    # turn file to sentences
+    sent_ds = ds.map(doc_to_sent, batched=True, batch_size=batch_size)
+
+    # apply forward pass
+    if batch_size is None:
+        batch_size = len(sent_ds)
+    sent_ds = sent_ds.map(_forward_pass, batch_size=batch_size)
+
+    # extract KG
--- utils.py
+++ utils.py
@@ -1,2 +1,127 @@
 """
 """
+
+import transformers
+import numpy as np
+
+
+def create_mapping(spacy_dict: dict, return_pytorch=False,
+                   tokenizer=transformers.AutoTokenizer.from_pretrained(
+                       "Maltehb/-l-ctra-danish-electra-small-cased")):
+    """
+    Creates mapping from token to id, token to tokenizer id
+    """
+
+    tokens = spacy_dict["token"]
+
+    chunk2id = {}
+
+    start_chunk = [chunk[0] for chunk in spacy_dict["noun_chunk_token_span"]]
+    end_chunk = [chunk[1] for chunk in spacy_dict["noun_chunk_token_span"]]
+    noun_chunks = spacy_dict["noun_chunk"]
+
+    sentence_mapping = []
+    token2id = {}
+    mode = 0  # 1 in chunk, 0 not in chunk
+    chunk_id = 0
+    for idx, token in enumerate(tokens):
+        if idx in start_chunk:
+            mode = 1
+            sentence_mapping.append(noun_chunks[chunk_id])
+            token2id[sentence_mapping[-1]] = len(token2id)
+            chunk_id += 1
+        elif idx in end_chunk:
+            mode = 0
+
+        if mode == 0:
+            sentence_mapping.append(token)
+            token2id[sentence_mapping[-1]] = len(token2id)
+
+    token_ids = []
+    tokenid2word_mapping = []
+
+    for token in sentence_mapping:
+        subtoken_ids = tokenizer(str(token),
+                                 add_special_tokens=False)['input_ids']
+        tokenid2word_mapping += [token2id[token]]*len(subtoken_ids)
+        token_ids += subtoken_ids
+
+    tokenizer_name = str(tokenizer.__str__)
+    if 'GPT2' in tokenizer_name:
+        outputs = {
+            'input_ids': token_ids,
+            'attention_mask': [1]*(len(token_ids)),
+        }
+
+    else:
+        outputs = {
+            'input_ids': [tokenizer.cls_token_id] + token_ids + [tokenizer.sep_token_id],
+            'attention_mask': [1]*(len(token_ids)+2),
+            'token_type_ids': [0]*(len(token_ids)+2)
+        }
+
+    if return_pytorch:
+        for key, value in outputs.items():
+            outputs[key] = torch.from_numpy(
+                np.array(value)).long().unsqueeze(0)
+
+    return outputs, tokenid2word_mapping, token2id, noun_chunks
+
+
+def forward_pass(texts: list, tokenizer, model, **kwargs):
+    """
+    moves data to model device so model should be placed in the
+    desired device
+
+    >>> tokenizer = transformers.AutoTokenizer.from_pretrained(
+                       "Maltehb/-l-ctra-danish-electra-small-cased")
+    >>> model = transformers.ElectraModel.from_pretrained(
+        "Maltehb/-l-ctra-danish-electra-small-cased")
+    >>> res = forward_pass(["dette er en eksempel texts"], tokenizer, model)
+    """
+    input_ = tokenizer(texts, return_tensors="pt", **kwargs)
+    input_.to(model.device)
+    output = model(**input_, output_attentions=True)
+    # output[0].shape # batch, seq. length, embedding size
+    return {"attention": output.attentions[-1],
+            "embedding": output[0]}
+
+
+def compress_attention(attention, tokenid2word_mapping, operator=np.mean):
+
+    new_index = []
+
+    prev = -1
+    for idx, row in enumerate(attention):
+        token_id = tokenid2word_mapping[idx]
+        if token_id != prev:
+            new_index.append([row])
+            prev = token_id
+        else:
+            new_index[-1].append(row)
+
+    new_matrix = []
+    for row in new_index:
+        new_matrix.append(operator(np.array(row), 0))
+
+    new_matrix = np.array(new_matrix)
+
+    attention = np.array(new_matrix).T
+
+    prev = -1
+    new_index = []
+    for idx, row in enumerate(attention):
+        token_id = tokenid2word_mapping[idx]
+        if token_id != prev:
+            new_index.append([row])
+            prev = token_id
+        else:
+            new_index[-1].append(row)
+
+    new_matrix = []
+    for row in new_index:
+        new_matrix.append(operator(np.array(row), 0))
+
+    new_matrix = np.array(new_matrix)
+
+    return new_matrix.T
