ddbea895b0eea7a314fc49c61ed4b9cda0678f12
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-04-21 14:26:45 +0200

    added offload option to belief graph

--- belief_graph/BeliefGraph.py
+++ belief_graph/BeliefGraph.py
@@ -18,8 +18,11 @@ class BeliefGraph:
         parser: BeliefParser,
         triplet_filters: List[TripletFilter] = [],
         group_filters: List[TripletGroupFilter] = [],
+        offload_dir: Optional[str] = None
     ):
-        """"""
+        """
+        offload_dir: directory to write triplets span into. This will save on memory and allow you to save the graph
+        """
         if isinstance(triplet_filters, TripletFilter):
             triplet_filters = [triplet_filters]
         if isinstance(group_filters, TripletFilter):
@@ -30,6 +33,7 @@ class BeliefGraph:
         self.group_filters = group_filters
         self._triplet_heap = []
         self._triplet_group_heap = []
+        self.offload_dir = offload_dir
 
         self.is_sorted = True
 
@@ -37,6 +41,8 @@ class BeliefGraph:
     def add_belief_triplets(self, triplets: Iterable[BeliefTriplet]):
         self.is_sorted, self.is_filtered, self.is_merged = False, False, False
         for triplet in triplets:
+            if self.offload_dir:
+                triplet.offload()
             heappush(self._triplet_heap, triplet)
 
     def add_texts(self, texts: Union[Iterable[str], str]):
@@ -105,6 +111,7 @@ class BeliefGraph:
     def filtered_triplet_groups(self) -> Generator[TripletGroup, None, None]:
         triplet_filter = self.merge_filters(self.triplet_groups)
         return self.triplet_filter(self.triplet_groups)
+
 
 def merge_triplets(
     sorted_triplets: List[BeliefTriplet],
