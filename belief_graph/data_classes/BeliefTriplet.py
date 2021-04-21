54951fadab374b4e136a5f4855a088f58051529b
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-04-21 14:14:01 +0200

    added offload to triplets

--- belief_graph/data_classes/BeliefTriplet.py
+++ belief_graph/data_classes/BeliefTriplet.py
@@ -1,43 +1,80 @@
-"""
-This Script contains the Belief Triplet class
-"""
 from __future__ import annotations
 
+import os
+from pathlib import Path
 from typing import Callable, List, Optional, Tuple
 
 from pydantic import BaseModel
-from spacy.tokens import Span
+from spacy.tokens import Doc, Span
+from spacy.vocab import Vocab
 
+from .BeliefTriplet import BeliefTriplet
+
+
+class BaseTriplet(BaseModel):
+    """
+    The base class for other belief triplets.
+    """
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    head: str
+    tail: str
+    relation: str
+    confidence: Optional[float] = None
+    sentence: str
+
+    def __eq__(self, other: BaseTriplet) -> bool:
+        if (
+            (self.head == other.head)
+            and (self.tail == other.tail)
+            and (self.relation == other.relation)
+        ):
+            return True
+        return False
+
+    def __repr_str__(self, join_str: str) -> str:
+        return join_str.join(
+            repr(v) if a is None else f"{a}={v!r}"
+            for a, v in [
+                ("head", self.head),
+                ("relation", self.relation),
+                ("tail", self.tail),
+                ("confidence", round(self.confidence, 2)),
+                ("span", self.sentence),
+            ]
+        )
+
+    def __lt__(self, other: BaseTriplet):
+        return self.head < other.head
+
+    def __gt__(self, other: BaseTriplet):
+        return self.head > other.head
 
 
 def text_getter(span: Span):
     return span.text
 
 
-class BeliefTriplet(BaseModel):
-    """
-    the data class for the belief triplet. Which contains besides
-
-    ht_getter ("lemma"|"text"): should it return the full head tail (text) or just the lemma (lemma).
-    """
-
+class BeliefTriplet(BaseTriplet):
     class Config:
         arbitrary_types_allowed = True
 
+    head: Optional[str] = None
+    tail: Optional[str] = None
+    relation: Optional[str] = None
+    sentence: Optional[str] = None
     head_id: int
     tail_id: int
     relation_ids: Tuple[int, ...]
     confidence: float
-    span: Optional[Span] = None
     getter: Callable = text_getter
-
-    @property
-    def tail(self) -> str:
-        return self.getter(self.tail_span)
-
-    @property
-    def head(self) -> str:
-        return self.getter(self.head_span)
+    is_offloaded: bool = False
+    _span_slice: Optional[slice] = None
+    _doc_reference: Optional[str] = None
+    __span: Optional[Span] = None
+    __vocab: Optional[Vocab] = None
 
     @property
     def tail_span(self) -> Span:
@@ -55,35 +92,92 @@ class BeliefTriplet(BaseModel):
         return [span._.nctokens[i] for i in self.relation_ids]
 
     @property
-    def relation(self) -> str:
+    def span(self) -> Span:
+        if self.__span:
+            return self.__span
+        doc = Doc(self.__vocab).from_disk(self.doc_reference)
+        self.__span = doc[self.span_slice]
+        return self.__span
+
+    @staticmethod
+    def from_parse(
+        self,
+        head_id: int,
+        tail_id: int,
+        relation_ids: Tuple[int, ...],
+        confidence: float,
+        span: Span,
+    ):
+
+        bt = BeliefTriplet(
+            head_id=head_id,
+            tail_id=tail_id,
+            relation_ids=relation_ids,
+            confidence=confidence,
+            span=span,
+        )
+        bt.update()
+        return bt
+
+    def set_getter(self, getter: Callable):
+        self.getter = getter
+        self.update()
+
+    def get_tail(self) -> str:
+        return self.getter(self.tail_span)
+
+    def get_head(self) -> str:
+        return self.getter(self.head_span)
+
+    def get_relation(self) -> str:
         return " ".join(self.getter(t) for t in self.relation_list)
 
-    def __eq__(self, other: BeliefTriplet) -> bool:
-        if (
-            (self.head == other.head)
-            and (self.tail == other.tail)
-            and (self.relation == other.relation)
-        ):
-            return True
-        return False
+    def get_sentence(self) -> str:
+        return self.span.text
 
-    def __repr_str__(self, join_str: str) -> str:
-        return join_str.join(
-            repr(v) if a is None else f"{a}={v!r}"
-            for a, v in [
-                ("head", self.head),
-                ("relation", self.relation),
-                ("tail", self.tail),
-                ("confidence", round(self.confidence, 2)),
-                ("span", self.span),
-            ]
-        )
+    def update(self):
+        self.sentence = self.get_sentence()
+        self.head = self.get_head()
+        self.tail = self.get_tail()
+        self.relation = self.get_relation()
 
-    def __lt__(self, other: BeliefTriplet):
-        return self.head < other.head
+    def clean(self):
+        self.__span = None
 
-    def __gt__(self, other: BeliefTriplet):
-        return self.head > other.head
+    def offload(
+        self,
+        vocab: Optional[Vocab] = None,
+        id_set: Optional[set] = None,
+        dir="triplet_docs",
+    ):
+        if self.is_offloaded:
+            return None
+
+        if vocab is not None:
+            self.vocab = vocab
+        if self.vocab is None:
+            raise ValueError(
+                "No vocab is specified. Writing a doc without a Vocab will lead to the doc not being properly recoverable"
+            )
+
+        span = self.__span
+        path = spacy_doc_to_dir(span.doc, id_set=id_set, dir=dir)
+
+        self._span_slice = slice(span.start, span.end)
+        self._doc_reference = path
+
+        self.clean()
 
 
+def spacy_doc_to_dir(doc, id_set: Optional[set] = None, dir="triplet_docs") -> str:
+    Path(dir).mkdir(parents=True, exist_ok=True)
 
+    if id_set is None:
+        id_set = set(f[:-4] for f in os.listdir(dir) if f.endswith(".doc"))
+
+    id_ = id(doc)
+    path = os.path.join(dir, id_ + ".doc")
+    if id_ not in id_set:
+        doc.to_disk(path)
+
+    return path
--- belief_graph/data_classes/TripletGroup.py
+++ belief_graph/data_classes/TripletGroup.py
@@ -1,43 +1,84 @@
 from __future__ import annotations
 
-from typing import List, Union
+from typing import List, Optional, Union
 
-from pydantic.main import UNTOUCHED_TYPES
 from spacy.tokens import Span
+from spacy.vocab import Vocab
 
-from .BeliefTriplet import BeliefTriplet
+from .BeliefTriplet import BaseTriplet, BeliefTriplet, spacy_doc_to_dir
 
 
-class TripletGroup(BeliefTriplet):
+class TripletGroup(BaseTriplet):
     """
     this is a triplet group for collapsing
     span is a list consisting of spacy spans or None
     """
+    class Config:
+        arbitrary_types_allowed = True
 
     confidence: List[float]
-    spans: List[Span]
+    sentence: List[str]
+    confidence: List[float]
+    __vocab: Optional[Vocab] = None
+    _span_slice: List[slice] = []
+    _doc_reference: List[str] = []
+    __span: List[Union[None, Span]] = []
+
 
     @property
     def count(self):
-        return len(self.spans)
+        return len(self.sentence)
 
     @staticmethod
-    def from_belief_triplet(triplet: BeliefTriplet) -> TripletGroup:
-        return TripletGroup(
-            head_id=triplet.head_id,
-            tail_id=triplet.tail_id,
-            relation_ids=triplet.relation_ids,
-            confidence=[triplet.confidence],
-            span=triplet.span,
-            spans=[triplet.span],
-            getter=triplet.getter,
+    def from_belief_triplet(triplet: BaseTriplet, offload=False) -> TripletGroup:
+        tg = TripletGroup(
+            head = triplet.head,
+            tail = triplet.tail,
+            sentence = [triplet.sentence],
+            confidence = [triplet.confidence]
         )
+        if not isinstance(triplet, BeliefTriplet):
+            return tg
+        if offload is True:
+            triplet.offload()
+
+        tg._span_slice.append(triplet._span_slice)
+        tg._doc_reference.append(triplet._doc_reference)
+
+        if triplet.is_offloaded is True:  # if it is offloaded
+            tg.__vocab = triplet.__vocab
+            tg.__span.append(None)
+        else:
+            tg.__span.append(triplet.span)
+
+    @property
+    def is_offloaded(self) -> bool:
+        return all(s is None for s in self.__span)
+
+    def offload(self, vocab: Optional[Vocab] = None, id_set: Optional[set] = None, dir="triplet_docs"):
+        if vocab is not None:
+            self.vocab = vocab
+        if self.vocab is None:
+            raise ValueError("No vocab is specified. Writing a doc without a Vocab will lead to the doc not being properly recoverable")
+        
+        for i, s in enumerate(self.__span):
+            if s is None:
+                continue
+            path = spacy_doc_to_dir(s.doc, id_set = id_set, dir=dir)
+            self._span_slice[i] = slice(s.start, s.end)
+            self._doc_reference[i] = path
 
     def add_triplet(self, triplet: BeliefTriplet):
-        self.spans.append(triplet.span)
+        self.sentence.apend(triplet.sentence)
+        self.confidence.append(triplet.confidence)
+        self._span_slice.append(triplet._span_slice)
+        self._doc_reference.append(triplet._doc_reference)
 
     def add_tg(self, triplet: TripletGroup):
-        self.spans += triplet.spans
+        self.sentence += triplet.sentence
+        self.confidence += triplet.confidence
+        self._span_slice += triplet._span_slice
+        self._doc_reference += triplet._doc_reference
 
     def __add(self, triplet: Union[BeliefTriplet, TripletGroup]):
         if isinstance(triplet, TripletGroup):
